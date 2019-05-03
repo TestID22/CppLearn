4ad29b278e163bdd7438ce76e081155a11f29115
September <vitaliiseptember@gmail.com>
2019-05-03 13:22:43 +0300

    cin+string

---
+++ Hello_Straustrup/Hello_Straustrup/Hello_Straustrup/Hello_Straustrup.cpp
@@ -0,0 +1,23 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	string firstmessage;
+	cout << "Hello, Straustrup!\n"; 
+	cout << "Передай привет Страусттрупу\n";
+	cin >> firstmessage;// ессли вводить Русские символы, то выводит "БЯКУ"
+	cout << "Привет, " << firstmessage << "!\n";
+	
+	
+}
+
