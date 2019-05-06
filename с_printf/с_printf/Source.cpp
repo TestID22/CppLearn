033224da5da6dcaf18990b2a286e73c1d0238192
September <vitaliiseptember@gmail.com>
2019-05-06 16:27:53 +0300

    work with debager

---
+++ с_printf/с_printf/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+	printf("Hello from C\n");
+	int a; // целочисленная переменная а
+	for (a = 0; a < 10; a++) { //цикл фор 
+
+		printf("Hello!\n"); // escape последовательность не подвсечивается в языке C
+	}
+	return 0;
+	
+	
+}
