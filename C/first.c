9323d65f17a83eb97193c2870b019b2f97da9a77
September <vitaliiseptember@gmail.com>
2019-05-17 13:29:24 +0300

    learn c_oh_again

---
+++ C/first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Верная программа, которая ничего не делает
+
+int main()
+{
+	printf("Hello World\n");
+
+	int a = 10;
+	char s = getchar();
+	if (s != EOF)
+
+}
---
+++ C/functions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int square (int a; int b;)
+{
+	return int a * int b
+}
+
+void main()
+{
+	int a = 10, b = 5;
+	int result;
+	result = square(a,b);
+	printf(square);
+	return 0;
+}
+
